16f4eeb487760ab9fda02c87cb37656641a41985
Sergey <in43sh@gmail.com>
2020-01-26 23:09:35 -0800

    Work on navbar.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import logo from '../../images/logo-new.jpg';
+import logo from '../../images/logo.jpg';
 
 function NavBar() {
     return (
         <div className="navbar">
-            <img className="img-fluid" src={logo} alt="company logo"/>
-            <p>(425) 260 2121</p>
+            <div className="navbar-mobile">
+                <img className="img-fluid" src={logo} alt="company logo"/>
+                <p>(425) 260 2121</p>
+            </div>
+            <div className="navbar-desktop">
+                <div className="navbar-desktop__logo-container">
+                    <img className="img-fluid" src={logo} alt="company logo"/>
+                </div>
+                <p>(425) 260 2121</p>
+            </div>
         </div>
     );
 }
