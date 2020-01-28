6e601486fa29ecd583cbdd8615078555044116ee
in43sh <in43sh@gmail.com>
2020-01-27 19:23:17 -0800

    Add links to navigation bar.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,10 +9,15 @@ function NavBar() {
                 <p>(425) 260 2121</p>
             </div>
             <div className="navbar-desktop">
-                <div className="navbar-desktop__logo-container">
+                <div className="navbar-desktop__logo-container pointer">
                     <img className="img-fluid" src={logo} alt="company logo"/>
                 </div>
                 <p>(425) 260 2121</p>
+                <a>Home</a>
+                <a>About Us</a>
+                <a>Reservation</a>
+                <a>Services</a>
+                <a>Contact Us</a>
             </div>
         </div>
     );
