753f63473b347a6e842cd90a3354e4169d29be3b
in43sh <in43sh@gmail.com>
2020-01-28 19:54:05 -0800

    Add Content component.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import NavBar from './components/NavBar/NavBar.js';
-import Footer from './components/Footer/Footer.js';
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import Content from './components/Content/Content';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-
+      <Content/>
       <Footer/>
     </div>
   );
---
+++ src/components/Content/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../images/logo.jpg';
+
+function Content() {
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default Content;
