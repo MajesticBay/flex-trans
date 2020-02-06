44102a16bdf21a9ea105068e95eb3c3f9cce81ba
in43sh <in43sh@gmail.com>
2020-02-05 20:18:27 -0800

    Work on top container.

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import ReservationForm from '../ReservationForm/ReservaitonForm';
+import fordtransit from '../../images/ford-transit-home-page.png';
 
 function Content() {
     return (
         <div className="content">
             <div className="background">
-                <p> </p>
+                <div className="content__floating-wrapper">
+                    <ReservationForm/>
+                    <div className="content__car-photo-wrapper">
+                        <img className="img-fluid" src={fordtransit} alt="Ford Transit Wheelchair Vagon"/>
+                    </div>
+                </div>
             </div>
         </div>
     );
---
+++ src/components/ReservationForm/ReservaitonForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ReservationForm() {
+    return (
+        <div className="reservation-form rounded">
+            <input className="reservation-form__input mb-16 rounded" placeholder="Pick-up location"/>
+            <input className="reservation-form__input mb-45 rounded" placeholder="Drop-off location"/>
+            <div className="reservation-form__submit-btn rounded pointer">
+                <span className="reservation-form__submit-text">Get instant quote for your trip</span>
+            </div>
+        </div>
+    );
+}
+
+export default ReservationForm;
