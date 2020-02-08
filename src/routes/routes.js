ce71d3ab42c559eaff38100797adf2fd3657956c
Sergey <in43sh@gmail.com>
2020-02-08 15:23:02 -0800

    Add Services and ContactUs pages.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,11 +4,15 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../components/Home/Home';
 import AboutUs from '../components/AboutUs/AboutUs';
 import Reservation from '../components/Reservation/Reservation';
+import Services from '../components/Services/Services';
+import ContactUs from '../components/ContactUs/ContactUs';
 
 export default (
     <Switch>
         <Route exact path="/" component={ Home }/>
         <Route path="/about" component={ AboutUs }/>
         <Route path="/reservation" component={ Reservation }/>
+        <Route path="/services" component={ Services }/>
+        <Route path="/contact" component={ ContactUs }/>
     </Switch>
 )
