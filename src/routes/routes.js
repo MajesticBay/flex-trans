76852a84d220f96993587c63d772333ac8db988c
Sergey <in43sh@gmail.com>
2020-02-08 15:10:50 -0800

    Add AboutUs page.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import { Route, Switch } from 'react-router-dom';
 import Home from '../components/Home/Home';
+import AboutUs from '../components/AboutUs/AboutUs';
 
 export default (
     <Switch>
         <Route exact path="/" component={ Home }/>
+        <Route path="/about" component={ AboutUs }/>
     </Switch>
 )
