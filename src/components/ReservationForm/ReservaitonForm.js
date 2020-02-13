367ab4c164e2daa30a14f7b6dafdccfbc0761c8d
in43sh <in43sh@gmail.com>
2020-02-12 19:52:23 -0800

    Work on FloatingCar responsiveness.

--- src/components/ReservationForm/ReservaitonForm.js
+++ src/components/ReservationForm/ReservaitonForm.js
@@ -3,8 +3,8 @@ import React from 'react';
 function ReservationForm() {
     return (
         <div className="reservation-form rounded">
-            <input className="reservation-form__input mb-16 rounded" placeholder="Pick-up location"/>
-            <input className="reservation-form__input mb-45 rounded" placeholder="Drop-off location"/>
+            <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
+            <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
             <div className="reservation-form__submit-btn rounded pointer">
                 <span className="reservation-form__submit-text">Get instant quote for your trip</span>
                 <div className="reservation-form__triangle"></div>
