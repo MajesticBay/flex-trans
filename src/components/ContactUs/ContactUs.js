81b60faee960224b27193e199ae2770e7f276246
Sergey <in43sh@gmail.com>
2020-02-18 21:02:27 -0700

    Work on contact us form: create input fields, give them some styling.

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -13,11 +13,30 @@ function ContactUs() {
         <h3 className="contact-us__header">Contact Information</h3>
         <p>Phone: 1-425-444-3905</p>
         <p>Email: flextranswa@gmail.com</p>
-        <div>
-          <p>Send us a message</p>
-          <div>
-            <p>First and Last Name</p>
-            <input className=""></input>
+        <div className="contact-us__contact-form rounded">
+          <p className="contact-us__header contact-us__header--form">Send us a message</p>
+          <div className="contact-form__contact-form-inside-wrapper">
+            <div className="contact-form-inside-wrapper__contact-left">
+              <div className="contact-form__contact-form-group">
+                <p>First and Last Name</p>
+                <input className="contact-form-group__input rounded"></input>
+              </div>
+              <div className="contact-form__contact-form-group">
+                <p>Phone</p>
+                <input className="contact-form-group__input rounded"></input>
+              </div>
+              <div className="contact-form__contact-form-group">
+                <p>Email</p>
+                <input className="contact-form-group__input rounded"></input>
+              </div>
+            </div>
+
+            <div className="contact-form-inside-wrapper__contact-right">
+              <div className="contact-us__contact-form-group">
+                <p>Message</p>
+                <textarea className="contact-form-group__textarea rounded"></textarea>
+              </div>
+            </div>
           </div>
         </div>
       </div>
