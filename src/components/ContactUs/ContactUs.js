0f3551225587dcaf7d3b708f6047a76b5cbbe965
Sergey <in43sh@gmail.com>
2020-02-18 20:29:27 -0700

    Work on ContactUs page.

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -4,14 +4,26 @@ import Top from '../Top/Top';
 import TalkingCar from '../TalkingCar/TalkingCar';
 import Mission from '../Mission/Mission';
 
-function Services() {
+function ContactUs() {
   return (
     <div className="contact-us">
       <Top/>
       <TalkingCar/>
+      <div className="contact-us__contact-us-content">
+        <h3 className="contact-us__header">Contact Information</h3>
+        <p>Phone: 1-425-444-3905</p>
+        <p>Email: flextranswa@gmail.com</p>
+        <div>
+          <p>Send us a message</p>
+          <div>
+            <p>First and Last Name</p>
+            <input className=""></input>
+          </div>
+        </div>
+      </div>
       <Mission/>
     </div>
   );
 }
 
-export default Services;
+export default ContactUs;
