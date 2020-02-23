5b643533aa425373e3edfd685de04ba7c79ecc94
Sergey <in43sh@gmail.com>
2020-02-22 19:32:04 -0800

    Make ReservationForm a class instead of function.

--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 import Top from '../Top/Top';
 import TalkingCar from '../TalkingCar/TalkingCar';
+import Mission from '../Mission/Mission';
 
-function Reservation() {
-  return (
-    <div className="reservation">
-      <Top/>
-      <TalkingCar/>
-    </div>
-  );
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="reservation">
+        <Top/>
+        <TalkingCar/>
+        <Mission/>
+      </div>
+    );
+  }
 }
 
 export default Reservation;
--- src/components/ReservationForm/ReservaitonForm.js
+++ src/components/ReservationForm/ReservaitonForm.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-function ReservationForm() {
-    return (
-        <div className="reservation-form rounded">
-            <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
-            <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
-            <div className="reservation-form__submit-btn rounded pointer">
-                <span className="reservation-form__submit-text">Get instant quote for your trip</span>
-                <div className="reservation-form__triangle"></div>
+class ReservationForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentStep: 1
+        }
+    }
+    render() {
+        return (
+            <div className="reservation-form rounded">
+                <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
+                <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
+                <div className="reservation-form__submit-btn rounded pointer">
+                    <span className="reservation-form__submit-text">Get instant quote for your trip</span>
+                    <div className="reservation-form__triangle"></div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default ReservationForm;
