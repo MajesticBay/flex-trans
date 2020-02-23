1f7cce3832eb64658328fa1992111be31030f6eb
Sergey <in43sh@gmail.com>
2020-02-22 21:02:32 -0800

    Add one more step.

--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -4,19 +4,14 @@ import Top from '../Top/Top';
 import TalkingCar from '../TalkingCar/TalkingCar';
 import Mission from '../Mission/Mission';
 
-class Reservation extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
-      <div className="reservation">
-        <Top/>
-        <TalkingCar/>
-        <Mission/>
-      </div>
-    );
-  }
+function Reservation(){
+  return (
+    <div className="reservation">
+      <Top/>
+      <TalkingCar/>
+      <Mission/>
+    </div>
+  );
 }
 
 export default Reservation;
--- src/components/ReservationForm/ReservaitonForm.js
+++ src/components/ReservationForm/ReservaitonForm.js
@@ -10,18 +10,30 @@ class ReservationForm extends React.Component {
 
     handleChange = event => {
         const {name, value} = event.target;
-        
+        this.setState({
+            [name]: value
+        })
     }
     render() {
         return (
-            <div className="reservation-form rounded">
-                <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
-                <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
-                <div className="reservation-form__submit-btn rounded pointer">
-                    <span className="reservation-form__submit-text">Get instant quote for your trip</span>
-                    <div className="reservation-form__triangle"></div>
+            <form className="reservation-form rounded">
+                <div className="reservation-form__step-one">
+                    <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
+                    <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
+                    <div className="reservation-form__submit-btn rounded pointer">
+                        <span className="reservation-form__submit-text">Get instant quote for your trip</span>
+                        <div className="reservation-form__triangle"></div>
+                    </div>
                 </div>
-            </div>
+                <div className="reservation-form__step-two">
+                    <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
+                    <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
+                    <div className="reservation-form__submit-btn rounded pointer">
+                        <span className="reservation-form__submit-text">Get instant quote for your trip</span>
+                        <div className="reservation-form__triangle"></div>
+                    </div>
+                </div>
+            </form>
         );
     }
 }
