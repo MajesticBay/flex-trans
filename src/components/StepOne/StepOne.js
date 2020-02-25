1ccc0b018e5daf8688a7d34171cf482562cea901
Sergey <in43sh@gmail.com>
2020-02-24 20:44:44 -0700

    Move TalkingCar.scss to StepOne.scss .

--- src/components/StepOne/StepOne.js
+++ src/components/StepOne/StepOne.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import fordtransit from '../../images/car-left-shadow.png';
 
 function StepOne(props) {
     if (props.currentStep !== 1) {
@@ -6,11 +7,16 @@ function StepOne(props) {
     }
     return (
         <div className="reservation-form__step-one">
-            <input className="reservation-form__input rounded" name="pick" placeholder="Pick-up location" onChange={props.handleChange}/>
-            <input className="reservation-form__input rounded" name="drop" placeholder="Drop-off location"/>
-            <div className="reservation-form__submit-btn rounded pointer">
-                <span className="reservation-form__submit-text">Get instant quote for your trip</span>
-                <div className="reservation-form__triangle"></div>
+            <div>
+                <input className="reservation-form__input rounded" name="pick" placeholder="Pick-up location" onChange={props.handleChange}/>
+                <input className="reservation-form__input rounded" name="drop" placeholder="Drop-off location"/>
+                <div className="reservation-form__submit-btn rounded pointer">
+                    <span className="reservation-form__submit-text">Get instant quote for your trip</span>
+                    <div className="reservation-form__triangle"></div>
+                </div>
+            </div>
+            <div className="step-one__car-photo-container">
+                <img className="img-fluid" src={fordtransit} alt="Ford Transit Wheelchair Vagon"/>
             </div>
         </div>
     )
