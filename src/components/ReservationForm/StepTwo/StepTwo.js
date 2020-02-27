adb70ab34b2e3e06da60d6285f1bd0a6106b0f50
in43sh <in43sh@gmail.com>
2020-02-27 15:28:22 -0800

    Work on StepTwo.

--- src/components/ReservationForm/StepTwo/StepTwo.js
+++ src/components/ReservationForm/StepTwo/StepTwo.js
@@ -6,8 +6,12 @@ function StepTwo(props) {
     }
     return (
         <div className="reservation-form__step-one">
-            <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
-            <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
+            <p>A: Seattle-Tacoma International Airport (SEA), International Boulevard, Seattle, WA, USA</p>
+            <p>B: Sammamish Landing Park, East Lake Sam- mamish Parkway Northeast, Sammamish, WA, USA</p>
+            <div>
+                <p>Wheelchair rampvan <br/> One way trip 43,5 mi</p>
+                <p>$53.31</p>
+            </div>
             <div className="reservation-form__submit-btn rounded pointer">
                 <span className="reservation-form__submit-text">Get instant quote for your trip Step2</span>
                 <div className="reservation-form__triangle"></div>
