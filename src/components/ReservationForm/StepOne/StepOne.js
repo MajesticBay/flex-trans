24b7f77fd70e08632aaf82b7a8a63574a2f620af
Sergey <in43sh@gmail.com>
2020-03-01 20:31:53 -0800

    Make StepOne class component.

--- src/components/ReservationForm/StepOne/StepOne.js
+++ src/components/ReservationForm/StepOne/StepOne.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import fordtransit from '../../../images/car-left-shadow.png';
+import { render } from 'node-sass';
 
-function StepOne(props) {
-    if (props.currentStep !== 1) {
-        return null
+class StepOne extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pickup: '',
+            drop: ''
+        }
     }
 
-    return (
-        <div className="reservation-form__step-one">
-            <div>
-                <div className="step-one__form rounded">
-                    <input className="reservation-form__input rounded" name="pick" placeholder="Pick-up location" onChange={props.handleChange}/>
-                    <input className="reservation-form__input rounded" name="drop" placeholder="Drop-off location"/>
-                    <div className="reservation-form__submit-btn rounded pointer">
-                        <span className="reservation-form__submit-text">Get instant quote for your trip</span>
-                        <div className="reservation-form__triangle"></div>
+    handleChange = event => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        })
+        console.log('name ', name, 'value', value);
+    }
+
+    render() {
+        if (props.currentStep !== 1) {
+            return null
+        }
+
+        return (
+            <div className="reservation-form__step-one">
+                <div>
+                    <div className="step-one__form rounded">
+                        <input className="reservation-form__input rounded" name="pick" placeholder="Pick-up location" onChange={props.handleChange}/>
+                        <input className="reservation-form__input rounded" name="drop" placeholder="Drop-off location"/>
+                        <div className="reservation-form__submit-btn rounded pointer">
+                            <span className="reservation-form__submit-text">Get instant quote for your trip</span>
+                            <div className="reservation-form__triangle"></div>
+                        </div>
                     </div>
                 </div>
+                <div className="step-one__car-photo-container">
+                    <img className="img-fluid" src={fordtransit} alt="Ford Transit Wheelchair Vagon"/>
+                </div>
             </div>
-            <div className="step-one__car-photo-container">
-                <img className="img-fluid" src={fordtransit} alt="Ford Transit Wheelchair Vagon"/>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default StepOne;
---
+++ src/components/ReservationForm/StepOne/StepOneFunctional.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import fordtransit from '../../../images/car-left-shadow.png';
+
+function StepOne(props) {
+    if (props.currentStep !== 1) {
+        return null
+    }
+
+    return (
+        <div className="reservation-form__step-one">
+            <div>
+                <div className="step-one__form rounded">
+                    <input className="reservation-form__input rounded" name="pick" placeholder="Pick-up location" onChange={props.handleChange}/>
+                    <input className="reservation-form__input rounded" name="drop" placeholder="Drop-off location"/>
+                    <div className="reservation-form__submit-btn rounded pointer">
+                        <span className="reservation-form__submit-text">Get instant quote for your trip</span>
+                        <div className="reservation-form__triangle"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="step-one__car-photo-container">
+                <img className="img-fluid" src={fordtransit} alt="Ford Transit Wheelchair Vagon"/>
+            </div>
+        </div>
+    )
+}
+
+export default StepOne;
