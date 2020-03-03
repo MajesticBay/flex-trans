541ce61d0bc12fa95e30347d352ecc61b5769d33
Sergey <in43sh@gmail.com>
2020-03-03 01:08:47 -0700

    Work on App.js .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import Mission from './components/Mission/Mission';
 import Footer from './components/Footer/Footer';
 import routes from './routes/routes';
+
 import './App.css';
 
+const DataContext = createContext();
+
 function App() {
+  const [test] = useState(data);
+  const [activeTest, setActiveTest] = useState(test[0]);
   return (
     <div className="App">
-      <NavBar/>
-      { routes }
-      <Mission/>
-      <Footer/>
+      <DataContext.Provider>
+        <NavBar/>
+        { routes }
+        <Mission/>
+        <Footer/>
+      </DataContext.Provider>
     </div>
   );
 }
