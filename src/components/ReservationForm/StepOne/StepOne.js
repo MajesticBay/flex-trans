a7f380dcb9847ddbfb10849e419200bc1e23f96f
Sergey <in43sh@gmail.com>
2020-03-05 21:41:39 -0700

    Work on StepThree.

--- src/components/ReservationForm/StepOne/StepOne.js
+++ src/components/ReservationForm/StepOne/StepOne.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import fordtransit from '../../../images/car-left-shadow.png';
+// import "https://maps.googleapis.com/maps/api/js?key=AIzaSyA1_FjAoDlmqOnFqr8ckuw9Ifw9T8oYgWU&libraries=places&callback=initMap";
+
+const google = window.google;
 
 class StepOne extends React.Component {
     constructor(props) {
         super(props);
+
+        // this.autocompleteInput = React.createRef();
+        // this.autocomplete = null;
+        // this.handlePlaceChanged = this.handlePlaceChanged.bind(this);
 
         this.state = {
             pickup: '',
             drop: ''
         }
     }
+
+    // componentDidMount() {
+    //     this.autocomplete = new google.maps.places.Autocomplete(this.autocompleteInput.current,
+    //         {"types": ["geocode"]});
+
+    //     this.autocomplete.addListener('place_changed', this.handlePlaceChanged);
+    // }
+
+    // handlePlaceChanged(){
+    //     const place = this.autocomplete.getPlace();
+    //     this.props.onPlaceLoaded(place);
+    // }
 
     handlePickupChange = event => {
         const {name, value} = event.target;
@@ -46,6 +65,7 @@ class StepOne extends React.Component {
             <div className="reservation-form__step-one">
                 <div>
                     <div className="step-one__form rounded">
+                        {/* <input className="reservation-form__input rounded" name="pickup" ref={this.autocompleteInput}  id="autocomplete" placeholder="Enter your address" type="text"/> */}
                         <input className="reservation-form__input rounded" name="pickup" placeholder="Pick-up location" onChange={this.handlePickupChange}/>
                         <input className="reservation-form__input rounded" name="drop" placeholder="Drop-off location" onChange={this.handleDropChange}/>
                         <div className="reservation-form__submit-btn rounded pointer" onClick={this.test}>
--- src/components/ReservationForm/StepThree/StepThree.js
+++ src/components/ReservationForm/StepThree/StepThree.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
+import map from '../../../images/map.png';
+
 function StepThree(props) {
     if (props.currentStep !== 3) {
         return null
     }
     return (
-        <div className="reservation-form__step-one">
-            <p>Pick-up date and time</p>
-            <p></p>
-            <input className="reservation-form__input rounded" placeholder="Date"/>
-            <input className="reservation-form__input rounded" placeholder="Time"/>
-            <input className="reservation-form__input rounded" placeholder="Time"/>
-            <div className="reservation-form__submit-btn rounded pointer">
-                <span className="reservation-form__submit-text">Step3</span>
-                <div className="reservation-form__triangle"></div>
+        <div className="reservation-form__step-three">
+            <div>
+                <p>Pick-up date and time</p>
+                <div>
+                    <span>Back</span>
+                    <span>Step 1</span>
+                    <span>of 5</span>
+                </div>
+                <div className="reservation-form__reservation-date-picker-container">
+                    <div className="reservation-date-picker-container__reservation-date-picker">
+                        <input className="reservation-form__input rounded" placeholder="Date"/>
+                        <div className="reservation-date-picker-container__reservation-time-picker">
+                            <input className="reservation-form__input rounded" placeholder="Time"/>
+                            <input className="reservation-form__input rounded" placeholder="Time"/>
+                        </div>
+                    </div>
+                    <div></div>
+                </div>
+                <div className="reservation-form__submit-btn rounded pointer">
+                    <span className="reservation-form__submit-text">Step3</span>
+                    <div className="reservation-form__triangle"></div>
+                </div>
             </div>
+            <div></div>
         </div>
     )
 }
