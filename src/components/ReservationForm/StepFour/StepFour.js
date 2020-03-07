2175fa8796ec83136f7548d58fa1904512d58245
Sergey <in43sh@gmail.com>
2020-03-07 00:17:02 -0700

    Add _next funciton to button on StepFour.

--- src/components/ReservationForm/StepFour/StepFour.js
+++ src/components/ReservationForm/StepFour/StepFour.js
@@ -8,7 +8,7 @@ function StepFour(props) {
         <div className="reservation-form__step-one">
             <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
             <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
-            <div className="reservation-form__submit-btn rounded pointer">
+            <div className="reservation-form__submit-btn rounded pointer" onClick={props.next()}>
                 <span className="reservation-form__submit-text">Step4</span>
                 <div className="reservation-form__triangle"></div>
             </div>
