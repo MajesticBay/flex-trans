b24cc545215089f0e9fd218813a0ce14a1fb7061
Sergey <in43sh@gmail.com>
2020-03-07 18:39:46 -0800

    Include ReservationFormProvider into the index.js file.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import ReservationFormProvider from './contexts/reservationFormContext';
 import './css/main.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
+ReactDOM.render(<Router>
+                    <ReservationFormProvider>
+                        <App/>
+                    </ReservationFormProvider>
+                </Router>
+                , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
