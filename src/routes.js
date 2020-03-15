db79351151a8a8f14523bd29fae2e6a51c8d4883
Sergey <in43sh@gmail.com>
2020-03-14 22:27:03 -0700

    Delete reservationFormTest from routes.

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import Home from './components/Home/Home';
 import AboutUs from './components/AboutUs/AboutUs';
 import Reservation from './components/Reservation/Reservation';
 import ContactUs from './components/ContactUs/ContactUs';
-import ReservationFormTest from './components/ReservationFormTest/MultiStepForm';
 
 export default (
     <Switch>
@@ -12,6 +11,5 @@ export default (
         <Route path="/about" component={ AboutUs }/>
         <Route path="/reserve" component={ Reservation }/>
         <Route path="/contact" component={ ContactUs }/>
-        <Route path="/test" component={ ReservationFormTest }/>
     </Switch>
 )
