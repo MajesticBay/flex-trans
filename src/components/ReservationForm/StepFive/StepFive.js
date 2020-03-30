ecd0ba6e4cdb284efe2266c935c177bced667f58
Sergey <in43sh@gmail.com>
2020-03-29 23:51:51 -0700

    Start working on StepFive.

--- src/components/ReservationForm/StepFive/StepFive.js
+++ src/components/ReservationForm/StepFive/StepFive.js
@@ -1,13 +1,46 @@
 import React from 'react';
 
+import { reservationFormContext } from '../../../contexts/reservationFormContext';
+
 function StepFive(props) {
+    const { addressPick} = React.useContext(reservationFormContext);
+    const { addressDrop } = React.useContext(reservationFormContext);
+    let mapUrl = `https://www.google.com/maps/embed/v1/directions?origin=${addressPick}&destination=${addressDrop}&language=EN&key=AIzaSyA97rzK2Y0x79nYrp4ozU5NzB7acY8MASE`;
     return (
-        <div className="reservation-form__step-one">
-            <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
-            <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
-            <div className="reservation-form__submit-btn rounded pointer" onClick={() => props.next()}>
-                <span className="reservation-form__submit-text">Step5</span>
+        <div className="reservation-form__step-four">
+            <div className="reservation-form__inner-container">
+                <div className="step-four__step-four-header">
+                    <span className="step-four__header-text">Reservation form</span>
+                    <div>
+                        <span>← Back</span>
+                        <span>Step 2 of 5</span>
+                    </div>
+                </div>
+                <div className="step-four__step-four-form">
+                    <div className="step-four__pickup-container">
+                        <p>Pick-up location</p>
+                        <input className="reservation-form__input rounded"/>
+                    </div>
+                    <div className="step-four__dropoff-container">
+                        <p>Drop-off location</p>
+                        <input className="reservation-form__input rounded"/>
+                    </div>
+                    <div className="step-four__dropoff-container">
+                        <p>Drop-off location</p>
+                        <input className="reservation-form__input rounded"/>
+                    </div>
+                </div>
+                <div className="step-four__step-four-footer">
+                    <div>
+                        <span>Total</span>
+                        <span>$0</span>
+                    </div>
+                    <div className="reservation-form__submit-btn rounded pointer" onClick={props.next}>
+                        <span className="reservation-form__submit-text">Next Step →</span>
+                    </div>
+                </div>
             </div>
+            <iframe title="google-map" frameBorder="0" style={{ width: "100%", height: "100%", borderRadius: "6px" }} src={mapUrl}></iframe>
         </div>
     )
 }
