219b8d4c392134d574b380c4a948c7830e5fc910
Sergey <in43sh@gmail.com>
2020-03-29 21:08:14 -0700

    Create markup for StepFour.

--- src/components/ReservationForm/StepFour/StepFour.js
+++ src/components/ReservationForm/StepFour/StepFour.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
+import { reservationFormContext } from '../../../contexts/reservationFormContext';
+
 function StepFour(props) {
+    const { addressPick} = React.useContext(reservationFormContext);
+    const { addressDrop } = React.useContext(reservationFormContext);
+    let mapUrl = `https://www.google.com/maps/embed/v1/directions?origin=${addressPick}&destination=${addressDrop}&language=EN&key=AIzaSyA97rzK2Y0x79nYrp4ozU5NzB7acY8MASE`;
     return (
-        <div className="reservation-form__step-one">
-            <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
-            <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
-            <div className="reservation-form__submit-btn rounded pointer" onClick={() => props.next()}>
-                <span className="reservation-form__submit-text">Step4</span>
+        <div className="reservation-form__step-four">
+            <div>
+                <div className="step-four__step-four-header">
+                    <span>Reservation form</span>
+                    <div>
+                        <span>← Back</span>
+                        <span>Step 2 of 5</span>
+                    </div>
+                </div>
+                <div className="step-four__step-four-form">
+                    <div>
+                    
+                    </div>
+                </div>
+                <div className="step-four__step-four-footer">
+                </div>
             </div>
+            <iframe title="google-map" frameBorder="0" style={{ width: "100%", height: "100%", borderRadius: "6px" }} src={mapUrl}></iframe>
         </div>
     )
 }
--- src/components/ReservationForm/StepThree/StepThree.js
+++ src/components/ReservationForm/StepThree/StepThree.js
@@ -27,7 +27,7 @@ function StepThree(props) {
                     </span>
                 </div>
                 <div className="step-three__reservation-date-picker-container">
-                    <input className="reservation-date-picker-container__reservation-date-picker reservation-form__input reservation-form__input--no-margin rounded" placeholder="Date" onChange={e => handleDataChange(e.target.value)}/>
+                    <input className="reservation-date-picker-container__reservation-date-picker reservation-form__input reservation-form__input--no-margin rounded" type="date" placeholder="Date" onChange={e => handleDataChange(e.target.value)}/>
                     <div className="reservation-date-picker-container__reservation-time-picker">
                         <input className="reservation-form__input reservation-form__input--no-margin rounded" placeholder="Time"/>
                         <span> : </span>
