536d8090e2dd074579603548fa2bfe32c87ff0f6
Sergey <in43sh@gmail.com>
2020-03-30 22:47:45 -0700

    Work on StepSix.

--- src/components/ReservationForm/StepSix/StepSix.js
+++ src/components/ReservationForm/StepSix/StepSix.js
@@ -2,11 +2,32 @@ import React from 'react';
 
 function StepSix(props) {
     return (
-        <div className="reservation-form__step-one">
-            <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
-            <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
-            <div className="reservation-form__submit-btn rounded pointer" onClick={() => props.next()}>
-                <span className="reservation-form__submit-text">Step6</span>
+        <div className="reservation-form__step-six">
+            <div className="step-six__inner-container">
+                <div className="step-six__step-six-header">
+                    <span className="step-six__header-text">Reservation form</span>
+                    <div>
+                        <span>← Back</span>
+                        <span>Step 2 of 5</span>
+                    </div>
+                </div>
+                <div className="step-six__step-six-content">
+                    <div>
+                    
+                    </div>
+                    <div>
+                    
+                    </div>
+                </div>
+                <div className="step-six__step-six-footer">
+                    <div>
+                        <span>Total</span>
+                        <span>$0</span>
+                    </div>
+                    <div className="reservation-form__submit-btn rounded pointer" onClick={props.next}>
+                        <span className="reservation-form__submit-text">Next Step →</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
