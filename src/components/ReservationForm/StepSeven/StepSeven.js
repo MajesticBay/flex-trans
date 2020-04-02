db46b92a8592e3dde6e5f50f20cba56b817cc4d3
Sergey <in43sh@gmail.com>
2020-04-01 23:41:07 -0700

    Start working on StepSeven.

--- src/components/ReservationForm/StepSeven/StepSeven.js
+++ src/components/ReservationForm/StepSeven/StepSeven.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { reservationFormContext } from '../../../contexts/reservationFormContext';
 
 function StepSeven(props) {
+    const { addressPick} = React.useContext(reservationFormContext);
+    const { addressDrop } = React.useContext(reservationFormContext);
+    const { distance } = React.useContext(reservationFormContext);
+    let mapUrl = `https://www.google.com/maps/embed/v1/directions?origin=${addressPick}&destination=${addressDrop}&language=EN&key=AIzaSyA97rzK2Y0x79nYrp4ozU5NzB7acY8MASE`;
     return (
-        <div className="reservation-form__step-one">
-            <input className="reservation-form__input rounded" placeholder="Pick-up location"/>
-            <input className="reservation-form__input rounded" placeholder="Drop-off location"/>
-            <div className="reservation-form__submit-btn rounded pointer" onClick={() => props.next()}>
-                <span className="reservation-form__submit-text">Step7</span>
+        <div className="reservation-form__step-seven">
+            <div className="step-seven__form rounded">
+                    <span className="step-seven__thank-you-text">Thank you for your reservation!  Our manager will be  in touch with you soon.</span>
+                    <div className="reservation-form__submit-btn rounded pointer" onClick={props.next}>
+                        <span className="reservation-form__submit-text">Reserve Another Trip →</span>
+                    </div>
+                </div>
+            <div className="step-one__car-photo-container">
+                <iframe title="google-map" frameBorder="0" style={{ width: "100%", height: "100%", borderRadius: "6px" }} src={mapUrl}></iframe>
             </div>
         </div>
     )
