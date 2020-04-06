98acc3a9990f6e565d813bb9333d30de2890910a
Sergey <in43sh@gmail.com>
2020-04-05 22:18:49 -0700

    Make a car for the top component for the mobile version.

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import car from '../images/car-left-shadow.png';
 
 function Top() {
     return (
         <div className="top">
+            <img className="top__car-image" src={car}/>
         </div>
     );
 }
