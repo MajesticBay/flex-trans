9ca4ad57cba7f3a5c918105d2ed52ea8db20262e
Sergey <in43sh@gmail.com>
2020-04-05 19:21:08 -0700

    Add WIP logging for requests

--- server/server.js
+++ server/server.js
@@ -2,9 +2,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const {log} = require("./logging");
 // const jsonParser = require('body-parser').json();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../build')));
+
+app.use((req, res, next) => {
+    log(req.ip + " has requested " + req.url);
+    next();
+})
 
 // const distance = require('google-distance-matrix');
 
