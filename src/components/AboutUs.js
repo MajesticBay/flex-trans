55e7d8a1821006197aa0d7fca6e5d8874ed17373
Sergey <in43sh@gmail.com>
2020-04-08 20:32:25 -0700

    Make images on AboutUs page have rounded corners only on desktop.

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -20,15 +20,15 @@ function AboutUs() {
         <h3 className="about-us__header">We provide Services</h3>
         <div className="about-us-services__services-container">
           <div>
-            <img className="img-fluid" src={aboutUsOne} alt="Transportation to Doctor's appointment"/>
+            <img className="img-fluid rounded-desktop" src={aboutUsOne} alt="Transportation to Doctor's appointment"/>
             <p className="services-container__service-header">Transportation to<br/>Doctor's appointment</p>
           </div>
           <div>
-            <img className="img-fluid" src={aboutUsTwo} alt="Transportation from Hospital to Home"/>
+            <img className="img-fluid rounded-desktop" src={aboutUsTwo} alt="Transportation from Hospital to Home"/>
             <p className="services-container__service-header">Transportation from<br/>Hospital to Home</p>
           </div>
           <div>
-            <img className="img-fluid" src={aboutUsThree} alt="Non-Emergency Wheelchair Transportation"/>
+            <img className="img-fluid rounded-desktop" src={aboutUsThree} alt="Non-Emergency Wheelchair Transportation"/>
             <p className="services-container__service-header">Non-Emergency<br/>Wheelchair Transportation</p>
           </div>
         </div>
