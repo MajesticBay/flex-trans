883b13ad98afe59ad4fba59a4e00ac0fa2362b6b
Sergey <in43sh@gmail.com>
2020-04-12 17:30:31 -0700

    Work on WelcomeWithoutMap styling.

--- src/components/WelcomeWithoutMap.js
+++ src/components/WelcomeWithoutMap.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import car from '../images/car-left.png';
+import car from '../images/ford-transit-right.png';
 
 function WelcomeWithoutMap() {
     return (
         <div className="welcome-without-map__container rounded-desktop">
             <div className="welcome-without-map__content">
-                <h3>Next Day Pick Up</h3>
-                <p>You may set up a ride at least 24 hours in advance. If you need to be picked up sooner, please call us at 425-260-2121. We do make exceptions.</p>
+                <h3 className="welcome-without-map__header">Next Day Pick Up</h3>
+                <p className="welcome-without-map__text">You may set up a ride at least 24 hours in advance. If you need to be picked up sooner, please call us at 425-260-2121. We do make exceptions.</p>
             </div>
             <div className="welcome-without-map__car-container">
                 <img className="img-fluid" src={car}/>
