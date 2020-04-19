8f84c9e22417aec51d27f1c82e26c738c3927a58
Sergey <in43sh@gmail.com>
2020-04-18 23:12:54 -0700

    Start working on coloring NavBar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo-cabulance.png';
 import phone from '../images/phone.svg';
 
 function NavBar() {
+    let isActive = true;
+    const [active, setActive] = useState("/");
+    let page = (window.location.href.split('/')[3]);
+    // console.log(page);
+    useEffect(() => {
+        setActive(page);
+        console.log(active);
+    })
+
+    const test = () => {
+        console.log(window.location.href);
+    }
+
     return (
         <div className="navbar">
             <div className="navbar-mobile">
@@ -43,10 +56,11 @@ function NavBar() {
                     </a>
                 </div>
                 <div className="navbar__navbar-links">
-                    <Link className="navbar-links__navbar-link" to="/">Home</Link>
+                    <Link className={` ${isActive} ? "backgroundColor: red" : "" navbar-links__navbar-link`} to="/">Home</Link>
                     <Link className="navbar-links__navbar-link" to="/about">About Us</Link>
                     <Link className="navbar-links__navbar-link" to="/reserve">Reserve a trip</Link>
                     <Link className="navbar-links__navbar-link" to="/contact">Contact Us</Link>
+                    <span onClick={() => test()}>test</span>
                 </div>
             </div>
         </div>
