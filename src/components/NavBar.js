94dfc99392631769e0e0f2843b3ce24b25f5c30c
Sergey <in43sh@gmail.com>
2020-04-19 22:16:19 -0700

    Work on highlighting the current page on the Navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,52 @@ function NavBar() {
     let isActive = true;
     const [active, setActive] = useState("/");
     let page = (window.location.href.split('/')[3]);
+    let isHomeActive = true;
+    let isAboutActive = false;
+    let isReservationActive = false;
+    let isContactActive = false;
     // console.log(page);
     useEffect(() => {
         setActive(page);
         console.log(active);
+        if (active == "/") {
+            
+        }
     })
 
-    const test = () => {
-        console.log(window.location.href);
+    const setActiveNavItem = () => {
+        page = (window.location.href.split('/')[3]);
+        setActive(page);
+        switch (active) {
+            case "":
+                {
+                    isHomeActive = true;
+                    isAboutActive = false;
+                    isReservationActive = false;
+                    isContactActive = false;
+                }
+            case "about":
+                {
+                    isHomeActive = false;
+                    isAboutActive = true;
+                    isReservationActive = false;
+                    isContactActive = false;
+                }
+            case "reserve":
+                {
+                    isHomeActive = false;
+                    isAboutActive = false;
+                    isReservationActive = true;
+                    isContactActive = false;
+                }
+            case "contact":
+                {
+                    isHomeActive = false;
+                    isAboutActive = false;
+                    isReservationActive = false;
+                    isContactActive = true;
+                }
+        }
     }
 
     return (
@@ -56,10 +94,10 @@ function NavBar() {
                     </a>
                 </div>
                 <div className="navbar__navbar-links">
-                    <Link className={` ${isActive} ? "backgroundColor: red" : "" navbar-links__navbar-link`} to="/">Home</Link>
-                    <Link className="navbar-links__navbar-link" to="/about">About Us</Link>
-                    <Link className="navbar-links__navbar-link" to="/reserve">Reserve a trip</Link>
-                    <Link className="navbar-links__navbar-link" to="/contact">Contact Us</Link>
+                    <Link style={{ color : isHomeActive ? 'red' : 'black' }} className="navbar-links__navbar-link" to="/" onClick={() => setActiveNavItem()}>Home</Link>
+                    <Link style={{ color : isAboutActive ? 'red' : 'black' }} className="navbar-links__navbar-link" to="/about" onClick={() => setActiveNavItem()}>About Us</Link>
+                    <Link style={{ color : isReservationActive ? 'red' : 'black' }} className="navbar-links__navbar-link" to="/reserve" onClick={() => setActiveNavItem()}>Reserve a trip</Link>
+                    <Link style={{ color : isContactActive ? 'red' : 'black' }} className="navbar-links__navbar-link" to="/contact" onClick={() => setActiveNavItem()}>Contact Us</Link>
                     {/* <span onClick={() => test()}>test</span> */}
                 </div>
             </div>
--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ReservationForm from './ReservationForm/ReservationForm';
 import WelcomeWithoutMap from './WelcomeWithoutMap';
 
 function Reservation(){
   return (
     <div className="reservation">
+      {/* <ReservationForm/> */}
       <WelcomeWithoutMap/>
     </div>
   );
