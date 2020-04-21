ab89c8cb06973f19ef6a859a43407e6d2c91998c
Sergey <in43sh@gmail.com>
2020-04-20 22:00:56 -0700

    Try to force refresh the page when click on NavBar item.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../images/logo-cabulance.png';
 import phone from '../images/phone.svg';
 
+// function useForceUpdate(){
+//     const [value, setValue] = useState(0); // integer state
+//     return () => setValue(value => ++value); // update the state to force render
+// }
+
 function NavBar() {
+    // const forceUpdate = useForceUpdate();
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+
+    const handleClick = () => {
+        forceUpdate();
+    }
+    
     return (
         <div className="navbar">
             <div className="navbar-mobile">
@@ -43,26 +55,26 @@ function NavBar() {
                     </a>
                 </div>
                 <div className="navbar__navbar-links">
-                    <NavLink activeStyle={{
-                        backgroundColor: 'rgb(223, 44, 33)',
-                        color: 'white'
-                        ,textDecoration: 'none'
-                    }} exact className="navbar-links__navbar-link" to="/">Home</NavLink>
-                    <NavLink activeStyle={{
-                        backgroundColor: 'rgb(223, 44, 33)',
-                        color: 'white'
-                        ,textDecoration: 'none'
-                    }} className="navbar-links__navbar-link" to="/about">About Us</NavLink>
-                    <NavLink activeStyle={{
-                        backgroundColor: 'rgb(223, 44, 33)',
-                        color: 'white'
-                        ,textDecoration: 'none'
-                    }} className="navbar-links__navbar-link" to="/reserve">Reserve a trip</NavLink>
-                    <NavLink activeStyle={{
-                        backgroundColor: 'rgb(223, 44, 33)',
-                        color: 'white'
-                        ,textDecoration: 'none'
-                    }} className="navbar-links__navbar-link" to="/contact">Contact Us</NavLink>
+                    <NavLink
+                        activeStyle={{ backgroundColor: 'rgb(223, 44, 33)', color: 'white', textDecoration: 'none'}}
+                        className="navbar-links__navbar-link" exact to="/"
+                        onClick={() => handleClick()}
+                        >Home</NavLink>
+                    <NavLink
+                        activeStyle={{ backgroundColor: 'rgb(223, 44, 33)', color: 'white', textDecoration: 'none'}}
+                        className="navbar-links__navbar-link" to="/about"
+                        onClick={() => handleClick()}
+                        >About Us</NavLink>
+                    <NavLink
+                        activeStyle={{ backgroundColor: 'rgb(223, 44, 33)', color: 'white', textDecoration: 'none'}}
+                        className="navbar-links__navbar-link" to="/reserve"
+                        onClick={() => handleClick()}
+                        >Reserve a trip</NavLink>
+                    <NavLink
+                        activeStyle={{ backgroundColor: 'rgb(223, 44, 33)', color: 'white', textDecoration: 'none'}}
+                        className="navbar-links__navbar-link" to="/contact"
+                        onClick={() => handleClick()}
+                        >Contact Us</NavLink>
                 </div>
             </div>
         </div>
