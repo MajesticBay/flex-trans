85bc845bf9412d9a479ebd35fbf8a739d79a3f50
Sergey <in43sh@gmail.com>
2020-04-21 20:41:48 -0700

    Change phone number on WelcomeWithoutMap container.

--- src/components/WelcomeWithoutMap.js
+++ src/components/WelcomeWithoutMap.js
@@ -7,7 +7,7 @@ function WelcomeWithoutMap() {
         <div className="welcome-without-map__container rounded-desktop">
             <div className="welcome-without-map__content">
                 <h3 className="welcome-without-map__header">Next Day Pick Up</h3>
-                <p className="welcome-without-map__text">You may set up a ride at least 24 hours in advance. If you need to be picked up sooner, please call us at 425-260-2121. We do make exceptions.</p>
+                <p className="welcome-without-map__text">You may set up a ride at least 24 hours in advance. If you need to be picked up sooner, please call us at 425-444-3905. We do make exceptions.</p>
             </div>
             <div className="welcome-without-map__car-container">
                 <img className="welcome-without-map__car-img img-fluid" src={car}/>
