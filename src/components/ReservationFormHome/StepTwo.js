3cdf8b070a0a1e6a8b0f51f715b2e59231501ca3
Sergey <in43sh@gmail.com>
2020-05-12 22:35:10 -0700

    Add margin-bottom for .reservation-form__reservation-footer-price-container, also start changing reservation form from display grid to display flex.

--- src/components/ReservationFormHome/StepTwo.js
+++ src/components/ReservationFormHome/StepTwo.js
@@ -33,7 +33,7 @@ function StepTwo(props) {
 
     return (
         <div className="reservation-form__step-two">
-            <div>
+            <div className="step-two__outer-container">
                 <div className="step-two__form rounded-desktop">
                     <div className="step-two__locations">
                         <span className="step-two__locations-text--red">A: </span><span className="step-two__locations-text">{addressPick}</span>
@@ -53,7 +53,7 @@ function StepTwo(props) {
                     </div>
                 </div>
             </div>
-            <div>
+            <div className="step-two__outer-container">
                 <iframe className="google-map-iframe" title="google-map" frameBorder="0" style={{ borderTopRightRadius: "6px", borderBottomRightRadius: "6px" }} src={mapUrl}></iframe>
             </div>
         </div>
